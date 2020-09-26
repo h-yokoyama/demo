1c48214098b5a972842ce5a70f2b1270eadff5df
h-yokoyama <milkmccya@gmail.com>
2020-09-26 20:39:06 +0900

    update pom.xml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.servlet.view.tiles3.SpringWildcardServletTilesApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
