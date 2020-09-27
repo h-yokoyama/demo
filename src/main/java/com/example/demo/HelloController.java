19ef9ee61a4341f8668454f9b9021ba960e5f41a
h-yokoyama <milkmccya@gmail.com>
2020-09-27 20:24:13 +0900

    commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	private String hello() {
-		return "Hello world! これはどうですか？";
+	@GetMapping("/hello")
+	private ResponseEntity<HttpStatus> hello() {
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/example/demo/service/CurrencyService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Currency;
+import com.example.demo.domain.CurrencyRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+
+    public List<Currency> findAll() {
+        return currencyRepository.findAll();
+    }
+    
+    public Currency save(String name, String symbol) {
+        return currencyRepository.save(Currency.newCurrency(name, symbol));
+    }
+    
+    public void delete(Long id) {
+        currencyRepository.findById(id).ifPresent(currency -> currencyRepository.delete(currency));
+    }
+}
